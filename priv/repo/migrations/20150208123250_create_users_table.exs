1a8ff512e0cc87a1731f8327dc9d657c8c241b2a
Ryan Kaskel <dev@ryankaskel.com>
2015-02-08 18:53:28 +0000

    Fix up migrations.

--- priv/repo/migrations/20150208123250_create_users_table.exs
+++ priv/repo/migrations/20150208123250_create_users_table.exs
@@ -1,18 +1,16 @@
 defmodule Marcy.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
-  def up do
+  def change do
     create table(:users) do
-      add :email, :varchar, size: 254, null: false, unique: true
+      add :email, :varchar, size: 254, null: false
       add :full_name, :varchar, size: 128
       add :short_name, :varchar, size: 30, null: false
       add :password, :char, size: 60
-      add :is_active, :boolean, default: true, null: false
-      add :is_admin, :boolean, default: false, null: false
+      add :is_active, :boolean, default: "true", null: false
+      add :is_admin, :boolean, default: "false", null: false
     end
-  end
 
-  def down do
-    drop table(:users)
+    create index(:users, [:email], unique: true)
   end
 end
--- priv/repo/migrations/20150208173817_create_posts_table.exs
+++ priv/repo/migrations/20150208173817_create_posts_table.exs
@@ -6,12 +6,13 @@ defmodule Marcy.Repo.Migrations.CreatePostsTable do
       add :user_id, references(:users), null: false
       add :status, :varchar, size: 28, null: false, default: "draft"
       add :title, :varchar, size: 255, null: false
-      add :slug, :varchar, size: 100, null: false, unique: true
+      add :slug, :varchar, size: 100, null: false
       add :body, :text
       add :tags, :"text[]"
       Marcy.Repo.Migration.timestamps
     end
 
+    create index(:posts, [:slug], unique: true)
     execute Marcy.Repo.Migration.modified_trigger_sql(:posts)
   end
 
