e2d960f5118f9a5c1c049ad65d5173684e0eb002
Ryan Kaskel <dev@ryankaskel.com>
2015-02-08 11:55:04 +0000

    Install ecto.

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Marcy.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {Marcy, []},
-     applications: [:phoenix, :cowboy, :logger]]
+     applications: [:phoenix, :cowboy, :logger, :postgrex, :ecto]]
   end
 
   # Specifies your project dependencies
@@ -23,6 +23,8 @@ defmodule Marcy.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "~> 0.8.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:postgrex, ">= 0.0.0"},
+     {:ecto, "~> 0.7"}]
   end
 end
