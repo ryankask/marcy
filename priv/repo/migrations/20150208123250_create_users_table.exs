268845e0b98dcaea8c4f52382473fe02ded73ad6
Ryan Kaskel <dev@ryankaskel.com>
2015-02-15 19:19:35 +0000

    Fix up migrations.

--- priv/repo/migrations/20150208123250_create_users_table.exs
+++ priv/repo/migrations/20150208123250_create_users_table.exs
@@ -1,16 +1,20 @@
 defmodule Marcy.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table(:users) do
       add :email, :varchar, size: 254, null: false
       add :full_name, :varchar, size: 128
       add :short_name, :varchar, size: 30, null: false
       add :password, :char, size: 60
-      add :is_active, :boolean, default: "true", null: false
-      add :is_admin, :boolean, default: "false", null: false
+      add :is_active, :boolean, default: true, null: false
+      add :is_admin, :boolean, default: false, null: false
     end
 
     create index(:users, [:email], unique: true)
+  end
+
+  def down do
+    drop table(:users)
   end
 end
